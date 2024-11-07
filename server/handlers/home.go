fee700bc12346ed45bb3614b0325e77dad202174
Hamza Maach <hamzamaach56@gmail.com>
2024-11-07 16:32:33 +0100

    Create Demo posts and display them in the home page

--- server/handlers/home.go
+++ server/handlers/home.go
@@ -4,7 +4,27 @@ import (
 	"log"
 	"net/http"
 
+	"forum/server/models"
 	"forum/server/utils"
+)
+
+var (
+	content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum cumque, voluptas dolore veniam excepturi aspernatur vero atque! Temporibus suscipit, excepturi, id corporis quod ea aliquam sapiente vitae eos reiciendis fugit."
+	Posts   = []models.Post{
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
+	}
 )
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +32,7 @@ func GetHome(w http.ResponseWriter, r *http.Request) {
 		utils.RenderError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	err := utils.RenderTemplate(w, "index", http.StatusOK, nil)
+	err := utils.RenderTemplate(w, "home", http.StatusOK, Posts)
 	if err != nil {
 		log.Println(err)
 		utils.RenderError(w, http.StatusInternalServerError)
--- server/models/post.go
+++ server/models/post.go
@@ -1,1 +1,11 @@
 package models
+
+type Post struct {
+	ID       int
+	UserName string
+	Content  string
+	Time     string
+	Likes    int
+	Dislikes int
+	Comments int
+}
