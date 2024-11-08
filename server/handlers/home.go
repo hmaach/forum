9b5577e193d2342ee56f362b50c192f47f693166
Hamza Maach <hamzamaach56@gmail.com>
2024-11-08 17:41:49 +0100

    create demo categories and excute them with home page

--- server/handlers/home.go
+++ server/handlers/home.go
@@ -8,6 +8,7 @@ import (
 	"forum/server/utils"
 )
 
+// Demo data for testing before the database is set up
 var (
 	content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum cumque, voluptas dolore veniam excepturi aspernatur vero atque! Temporibus suscipit, excepturi, id corporis quod ea aliquam sapiente vitae eos reiciendis fugit."
 	Posts   = []models.Post{
@@ -25,6 +26,19 @@ var (
 		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
 		{ID: 1, UserName: "Hamza Maach", Content: content, Time: "20:58 12/11/2024", Likes: 111, Dislikes: 60, Comments: 50},
 	}
+
+	Categories = []models.Category{
+		{ID: 1, Category: "politics"},
+		{ID: 2, Category: "sports"},
+		{ID: 3, Category: "fashion"},
+		{ID: 4, Category: "technologies"},
+		{ID: 4, Category: "technologies"},
+		{ID: 4, Category: "technologies"},
+		{ID: 4, Category: "technologies"},
+		{ID: 4, Category: "technologies"},
+		{ID: 4, Category: "technologies"},
+		{ID: 4, Category: "technologies"},
+	}
 )
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
@@ -33,12 +47,20 @@ func GetHome(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Limit categories to the first 6
+	limitedCategories := Categories
+	if len(Categories) > 6 {
+		limitedCategories = Categories[:6]
+	}
+
 	Data := struct {
 		IsAuthenticated bool
 		Posts           []models.Post
+		Categories      []models.Category
 	}{
 		IsAuthenticated: IsAuthenticated,
 		Posts:           Posts,
+		Categories:      limitedCategories,
 	}
 	err := utils.RenderTemplate(w, "home", http.StatusOK, Data)
 	if err != nil {
